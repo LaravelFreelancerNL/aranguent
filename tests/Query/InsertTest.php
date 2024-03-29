2ebe9479b5b24f56f81025b97d5530ddb44691ff
Bas <info@laravel-freelancer.nl>
2024-03-29 10:49:28 +0100

    style fix

--- tests/Query/InsertTest.php
+++ tests/Query/InsertTest.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\DB;
 use LaravelFreelancerNL\Aranguent\Testing\DatabaseTransactions;
 
 uses(
-    DatabaseTransactions::class
+    DatabaseTransactions::class,
 );
 
 test('insert', function () {
