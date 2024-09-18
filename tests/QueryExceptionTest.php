7a8cb1678deba4f8c8c5aac4858372f7f1313cd0
Bas <info@laravel-freelancer.nl>
2024-09-18 10:52:04 +0200

    Loosened exact error message matching to deal with us probably a version differences

--- tests/QueryExceptionTest.php
+++ tests/QueryExceptionTest.php
@@ -17,14 +17,8 @@ test('query exception extends illuminate', function () {
 });
 
 test('query exception has correct message', function () {
-    $this->expectExceptionMessage(
-        "400 - AQL: syntax error, unexpected identifier near 'this is not AQL' at position 1:1 (while parsing)"
-        . " (Connection: arangodb,AQL: this is not AQL - Bindings: array (\n"
-        . "  'testBind' => 'test',\n))",
-    );
-
     DB::execute('this is not AQL', ['testBind' => 'test']);
-});
+})->throws(QueryException::class, 'this is not AQL');
 
 test('query exception without binds', function () {
     expect(fn() => DB::execute("this is not AQL", []))->toThrow(QueryException::class);
