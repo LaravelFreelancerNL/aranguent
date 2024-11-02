64f396476c38c9107d8a4b92655603611c5d10e5
Bas <info@laravel-freelancer.nl>
2024-11-02 21:27:46 +0100

    Add castAsJson connection parameter

--- src/Testing/Concerns/InteractsWithDatabase.php
+++ src/Testing/Concerns/InteractsWithDatabase.php
@@ -14,9 +14,10 @@ trait InteractsWithDatabase
      * No cast is necessary as json is a first class citizen in ArangoDB.
      *
      * @param  array<mixed>|object|string  $value
+     * @param string|null $connection
      * @return \Illuminate\Contracts\Database\Query\Expression
      */
-    public function castAsJson($value)
+    public function castAsJson($value, $connection = null)
     {
         return DB::raw($value);
     }
