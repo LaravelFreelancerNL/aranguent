bb41c94dbb6979c5bc56eb7835297b7880bc81e7
Bas <info@laravel-freelancer.nl>
2024-12-05 14:13:30 +0100

    ci: ensure migrations are converted

--- bin/test.sh
+++ bin/test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 printf "\nRun tests\n"
+./vendor/bin/testbench convert:migrations
 ./vendor/bin/testbench migrate:fresh --drop-all --path=TestSetup/Database/Migrations --path=vendor/orchestra/testbench-core/laravel/migrations/ --realpath  --seed
 ./vendor/bin/testbench package:test --coverage --min=80 tests
