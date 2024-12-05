bea3a86167c772f378815a7f5020e9cfdaa6819a
Bas <info@laravel-freelancer.nl>
2024-12-05 13:55:42 +0100

    chore: added missing methods to the docblock for better autocompletion

--- src/Facades/Schema.php
+++ src/Facades/Schema.php
@@ -12,7 +12,7 @@ use LaravelFreelancerNL\Aranguent\Schema\Builder;
  * Table handling:
  *
  * @method static Builder create($collection, Closure $callback, $options = [])
- * @method static Builder getAllTables()
+ * @method static Builder getTables()
  * @method static Builder drop(string $collection)
  * @method static Builder dropIfExists(string $collection)
  * @method static Builder dropAllTables()
@@ -21,7 +21,9 @@ use LaravelFreelancerNL\Aranguent\Schema\Builder;
  *
  * View handling:
  * @method static Builder createView($name, array $properties, $type = 'arangosearch')
+ * @method static Builder hasView(string $name)
  * @method static Builder getView(string $name)
+ * @method static Builder getViews()
  * @method static Builder editView($name, array $properties)
  * @method static Builder renameView(string $from, string $to)
  * @method static Builder dropView(string $name)
@@ -29,10 +31,22 @@ use LaravelFreelancerNL\Aranguent\Schema\Builder;
  *
  * Analyzer handling:
  * @method static Builder createAnalyzer($name, array $properties)
+ * @method static Builder hasAnalyzer()
  * @method static Builder getAnalyzer(string $name)
+ * @method static Builder getAnalyzers()
  * @method static Builder replaceAnalyzer($name, array $properties)
  * @method static Builder dropAnalyzer(string $name)
  * @method static Builder dropAnalyzerIfExists(string $name)
+ * @method static Builder dropAllAnalyzers()
+ *
+ * Named Graph handling:
+ * @method static Builder creategraph(string $name, array $properties = [], bool $waitForSync = false)
+ * @method static Builder hasGraph(string $name)
+ * @method static Builder getGraph(string $name)
+ * @method static Builder getGraphs()
+ * @method static Builder dropGraph(string $name)
+ * @method static Builder dropGraphIfExists(string $name)
+ * @method static Builder dropAllGraphs()
  *
  * @see \LaravelFreelancerNL\Aranguent\Schema\Builder
  */
